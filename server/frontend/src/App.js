db8231f8414f63ca4cb60312503bb9ddcc60703e
Sylvain Perrenoud <sylvain.perrenoud@gmail.com>
2022-05-31 15:37:11 +0000

    fix IA

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,6 +7,20 @@ import ContactUs from './contactus.js';
 Copy the Web Chat code from Watson Assitant and paste it here
 */
 
+  window.watsonAssistantChatOptions = {
+      integrationID: "524a6060-ceda-4890-82bf-628831bda03d", // The ID of this integration.
+      region: "eu-de", // The region your integration is hosted in.
+      serviceInstanceID: "5ba7b1a0-0cca-403b-af03-d97a00af00da", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+    document.head.appendChild(t);
+  });
+
+
+
 class App extends React.Component {
 
   state = {
