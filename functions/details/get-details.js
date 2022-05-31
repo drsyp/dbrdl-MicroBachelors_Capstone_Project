1983351e9cc7f32dbed4f37b9baccfc2d4e2f2d5
drsyp <103826269+drsyp@users.noreply.github.com>
2022-05-31 13:35:26 +0200

    update FIX exports

--- functions/details/get-details.js
+++ functions/details/get-details.js
@@ -1,6 +1,3 @@
-/**
-* Get all dealerships
-*/
 const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
@@ -138,3 +135,4 @@ function getSlots(result) {
 
   return result;
 }
+exports.main = main;
